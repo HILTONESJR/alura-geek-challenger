1d463643052577c1394fefaa6a325e769da41c3e
Hilton Eleuterio dos Santos Junior <110138764+HILTONESJR@users.noreply.github.com>
2024-05-21 13:52:25 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ async function listaProdutos(){
     const conexao = await fetch("http://localhost:3000/produtos");
     const produtosConvertidos = conexao.json();
 
-    exibirNaTela(produtosConvertidos);
+    exibirNaTela(listaProdutos);
 }
 
 
